b9fe74f94ea45a234dc9aaff651e120d94f31f24
Łukasz Adamczak <lukasz@czak.pl>
2014-12-30 16:25:13 +0100

    Spanish implementation up to 1000000

--- src/test/java/pl/czak/num2words/TranslatorTest.java
+++ src/test/java/pl/czak/num2words/TranslatorTest.java
@@ -1,13 +1,29 @@
 package pl.czak.num2words;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
-import org.junit.Assert;
 
 public class TranslatorTest {
     
+    String t(int number) {
+        return Translator.translate(number);
+    }
+
     @Test
-    public void testTruth() {
-        Assert.assertTrue("This should be true", true);
+    public void testRegressions() {
+        assertEquals("ciento veintidós", t(122));
+        assertEquals("trescientos", t(300));
+        assertEquals("ochenta y seis", t(86));
+        assertEquals("cero", t(0));
+    }
+
+    @Test
+    public void testOverOneThousand() {
+        assertEquals("ochenta y seis mil ochenta y siete", t(86087));
+        assertEquals("mil", t(1000));
+        assertEquals("mil tres", t(1003));
+        assertEquals("un millón", t(1000000));
     }
 
 }

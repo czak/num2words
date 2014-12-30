72a6c6e3a319d79db163c0c401b184ae23d27a26
Łukasz Adamczak <lukasz@czak.pl>
2014-12-30 16:12:36 +0100

    Initial commit with a simple test & basic build.gradle

---
+++ src/test/java/pl/czak/num2words/TranslatorTest.java
@@ -0,0 +1,13 @@
+package pl.czak.num2words;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class TranslatorTest {
+    
+    @Test
+    public void testTruth() {
+        Assert.assertTrue("This should be true", true);
+    }
+
+}
